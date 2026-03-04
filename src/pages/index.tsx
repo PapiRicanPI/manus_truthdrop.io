57378cccef7d099bf420fa1e76926acb03864690
The Vault Archivist <TainoRican2n@gmail.com>
2026-03-04 18:09:04 +0800

    Fix: use explicit closing tags for Plausible scripts in Next.js Head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,8 +50,8 @@ export default function SplashPage() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;600;700&family=Courier+Prime:wght@400;700&display=swap" rel="stylesheet" />
         {/* Privacy-friendly analytics by Plausible */}
-        <script async src="https://plausible.io/js/pa-c9DR3lWAwJko8umb5bm2C.js" />
-        <script dangerouslySetInnerHTML={{ __html: `window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init()` }} />
+        <script async src="https://plausible.io/js/pa-c9DR3lWAwJko8umb5bm2C.js"></script>
+        <script dangerouslySetInnerHTML={{ __html: `window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init()` }}></script>
         <link rel="canonical" href="https://truthdrop.io/" />
         <meta name="robots" content="index, follow" />
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify([
